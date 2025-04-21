e678741b057d43c23329ce204d5c013d8922dccc
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2025-04-21 14:19:34 +0300

    Update users.php

--- MyProject/users.php
+++ MyProject/users.php
@@ -1,117 +1,33 @@
 <?php
-declare(strict_types=1);
 
-// Пространство имен
-namespace MyProject\Classes;
-
-// Регистрация автозагрузки классов
 spl_autoload_register(function ($class) {
-    $file = __DIR__ . '/' . str_replace('\\', '/', substr($class, 9)) . '.php';
-
+    $file = str_replace('\\', '/', __DIR__) . str_replace('\\', '/', substr($class,9)) . '.php'; 
+    // echo str_replace('\\', '/', __DIR__) . str_replace('\\', '/', substr($class,9)) . '.php';
+   
     if (file_exists($file)) {
         require $file;
+        
     }
 });
 
-// Интерфейс SuperUserInterface
-interface SuperUserInterface
-{
-    public function getInfo(): array;
-}
 
-// Класс User
-class User implements SuperUserInterface
-{
-    protected string $name;
-    protected string $login;
-    protected string $password;
+use MyProject\Classes\User;
+use MyProject\Classes\SuperUser;
 
-    // Статическая переменная для подсчета пользователей
-    private static int $userCount = 0;
 
-    public function __construct(string $name, string $login, string $password)
-    {
-        $this->name = $name;
-        $this->login = $login;
-        $this->password = $password;
-        self::$userCount++; // Увеличение счетчика при создании пользователя
-    }
+$user1 = new User('Polina', 'polina04', 'password1');
+$user2 = new User('Julia', 'juli08', 'password2');
+$user3 = new User('Toni', 'toni03', 'password3');
 
-    // Изменили метод showInfo() для вывода столбиком
-    public function showInfo(): void
-    {
-        echo "Имя: {$this->name}<br>Логин: {$this->login}<br><br>";
-    }
 
-    // Метод для получения информации о пользователе
-    public function getInfo(): array
-    {
-        return [
-            'имя пользователя' => $this->name,
-            'логин' => $this->login,
-            'пароль' => $this->password
-        ];
-    }
 
-    // Метод для получения количества пользователей
-    public static function getUserCount(): int
-    {
-        return self::$userCount;
-    }
-}
-
-// Класс SuperUser
-class SuperUser extends User implements SuperUserInterface
-{
-    protected string $role;
-
-    // Статическая переменная для подсчета суперпользователей
-    private static int $superUserCount = 0;
-
-    public function __construct(string $name, string $login, string $password, string $role)
-    {
-        parent::__construct($name, $login, $password);
-        $this->role = $role;
-        self::$superUserCount++; // Увеличение счетчика суперпользователей
-    }
-
-    // Изменили метод showInfo() для вывода столбиком
-    public function showInfo(): void
-    {
-        parent::showInfo();
-        echo "Роль: {$this->role}<br><br>";
-    }
-
-    // Реализация метода getInfo(), специфичного для суперпользователя
-    public function getInfo(): array
-    {
-        $info = parent::getInfo(); // Берем информацию от родительского класса
-        $info['роль'] = $this->role; // Добавляем роль
-        return $info;
-    }
-
-    // Метод для получения количества суперпользователей
-    public static function getSuperUserCount(): int
-    {
-        return self::$superUserCount;
-    }
-}
-
-// Создание нескольких пользователей
-$user1 = new User('Polina', 'pol123', 'password1');
-$user2 = new User('Julia', 'juli456', 'password2');
-$user3 = new User('Anna', 'anna789', 'password3');
-
-// Демонстрация информации о пользователях
 $user1->showInfo();
 $user2->showInfo();
 $user3->showInfo();
 
-// Создание суперпользователя
-$user = new SuperUser('Leo', 'leo525', 'password0', 'admin');
+$user = new SuperUser('Sergei', 'serg04', 'password0', 'admin');
 $user->showInfo();
-print_r($user->getInfo()); // Здесь вызываем getInfo() без ошибок
+$user->getInfo();
 
-// Использование публичных методов для получения количества пользователей
-echo "<p>Всего обычных пользователей: " . User::getUserCount() . "</p>\n";
-echo "<p>Всего cупер-пользователей: " . SuperUser::getSuperUserCount() . "</p>\n";
+echo "<p>Всего обычных пользователей: " . User::$userCount . "\n</p>";
+echo "<p>Всего cупер-пользователей: " . SuperUser::$superUserCount . "\n</p>";
