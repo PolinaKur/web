bbe3a582c88e796b98e577d16e998ab8706e385e
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2025-04-21 14:18:55 +0300

    Update User.php

--- MyProject/Classes/User.php
+++ MyProject/Classes/User.php
@@ -1,24 +1,51 @@
 <?php
-// User.php
+declare(strict_types=1);
 namespace MyProject\Classes;
+/*
+ * User Class, наследованный от абстрактного класса AbstractUser
+ */ 
 
-class User {
-    // Публичные свойства
-    public $name;
-    public $login;
+class User extends AbstractUser{ 
+    /**
+ * @param public $name - имя пользователя
+ *        public $login - логин
+ *        public $password - пароль
+ *        public static $userCount - количество пользователей
+ */
+    public $name; 
+    public $login; 
+    public $password;
+    public static $userCount = 0;
     
-    // Приватное свойство
-    private $password;
+    /*
+ * Функция вывода значение свойств класса
+ * @param string $name, string $login, string $password
+ * @return св-ва классов $this->name, $this->login, $this->password, $this->role
+ */	 
+    
+    function showInfo(){
+         echo "<p><b>name:</b> {$this->name} <b>login:</b> {$this->login} <b>password:</b> {$this->password}</p>";
+        
+     }
 
-    // Конструктор для инициализации свойств
-    public function __construct($name, $login, $password) {
-        $this->name = $name;
-        $this->login = $login;
-        $this->password = $password;
+    /*
+ * Функция конструктор
+ * @param string $name, string $login, string $password
+ * @return св-ва классов $this->name, $this->login, $this->password, $this->role, self::$userCount
+ */	 
+    function __construct($name, $login, $password){  //Конструктор
+      $this->name = $name;
+      $this->login = $login;
+      $this->password = $password;
+      self::$userCount++;
     }
 
-    // Метод для вывода информации о пользователе
-    public function showInfo() {
-        echo "Имя: {$this->name}, Логин: {$this->login}<br>";
+    /*
+ * Функция деструктор
+ * @param string $name, string $login, string $password
+ * @return вывод логина при удалении информации об элементах класса
+ */	  
+    function __destruct(){  // Деструктор
+      echo "<p>Пользователь <b>{$this->login}</b> удален </p>";
     }
 }
