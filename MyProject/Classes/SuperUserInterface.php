145185473fee77d07226bd9ac3da51d9d603577c
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2025-04-21 14:18:29 +0300

    Update SuperUserInterface.php

--- MyProject/Classes/SuperUserInterface.php
+++ MyProject/Classes/SuperUserInterface.php
@@ -1,8 +1,17 @@
 <?php
 declare(strict_types=1);
-
 namespace MyProject\Classes;
 
+/*
+ * SuperUserInterface Interface
+ */
 interface SuperUserInterface {
-    public function getInfo(): array; // Объявляем метод getInfo()
+        /*
+    * Функция вывода ассоциативного массива, 
+    * где в котором именами элементов массива являются имена свойств объекта,
+    * а значениями элементов - значения свойств объекта
+    * @param string $name, string $login, string $password
+    * @return св-ва классов $this->name, $this->login, $this->password, $this->role
+    */	
+    public function getInfo();
 }
