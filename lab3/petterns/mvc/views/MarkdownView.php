0abdfe225672f9f20a5a13b877db268cf734b80f
PolinaKur <126785905+PolinaKur@users.noreply.github.com>
2025-04-29 22:40:29 +0300

    Create MarkdownView.php

---
+++ lab3/petterns/mvc/views/MarkdownView.php
@@ -0,0 +1,17 @@
+<?php
+namespace patterns\mvc\views;
+
+class MarkdownView
+{
+    public function render(array $userData): string
+    {
+        $output = " Список пользователей\n\n";
+        
+        foreach ($userData as $user) {
+            $output .= " {$user['lastName']} {$user['firstName']}\n";
+            $output .= "- Email: {$user['email']}\n\n";
+        }
+        
+        return $output;
+    }
+}
